0e3144f5231cf73860535d895061ebfbd964c873
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-31 02:45:19 +0000

    Add slugify.rb module, extend and include module methods in user and item classes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :user
+  extend Slugify::ClassMethods
+  include Slugify::InstanceMethods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password
   has_secure_password
   has_many :items
+  extend Slugify::ClassMethods
+  include Slugify::InstanceMethods
 end
