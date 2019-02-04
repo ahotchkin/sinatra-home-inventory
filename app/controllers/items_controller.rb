48f3608fe1003f933d7fb1665a1b917427c11ac5
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-04 22:01:48 +0000

    Update how items are displayed on index page; Add restrictions to post '/items' route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,24 @@ class ItemsController < ApplicationController
     erb :'/items/index'
   end
 
-  get '/new' do
+  get '/items/new' do
     if logged_in?
       erb :'/items/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/items' do
+    if logged_in? && params[:item][:name] != "" && params[:item][:cost] != ""
+      item = Item.create(params[:item])
+      redirect '/items'
+    elsif logged_in? && params[:item][:name] == ""
+      # flash message to add a name
+      redirect '/items/new'
+    elsif logged_in? && params[:item][:cost] == ""
+      # flash message to add a cost
+      redirect '/items/new'
     else
       redirect '/login'
     end
