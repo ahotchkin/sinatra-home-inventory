4765c87ad5b53a6d8bed0ea64411ca4a3add7e40
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-04 16:54:10 +0000

    Add post '/signup' route to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'users/new'
+    if logged_in?
+      erb :'users/new'
+    else
+      redirect '/signup'
+      # does this need to be '/users/signup'?
+    end
+  end
+
+  post '/signup' do
+    user = User.new(params[user])
   end
 
 end
