a8a8bdf561d38505a37701c49f7dd57f37f57e31
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-11 20:42:06 +0000

    Add categories table and item_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211172923) do
+ActiveRecord::Schema.define(version: 20190211204022) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "item_categories", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "category_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string  "name"
