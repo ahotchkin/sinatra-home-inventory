605f2cfa6ba4dcf626cdcc5e0f67c1eeba2c18a4
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-18 02:20:18 +0000

    Reset database

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# Add seed data here. Seed your database with `rake db:seed`
-
-# Group.create(name: "Kitchen")
-# Group.create(name: "Living Room")
-# Group.create(name: "Bedroom")
-# Group.create(name: "Office")
-# Group.create(name: "Electronics")
-# Group.create(name: "Media")
-# Group.create(name: "Appliances")
