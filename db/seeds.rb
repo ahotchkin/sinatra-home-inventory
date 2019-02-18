a0c7084f29cff8106ce39b6f88df8ef477e337fd
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-18 02:06:09 +0000

    Remove seeds from database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-Group.create(name: "Kitchen")
-Group.create(name: "Living Room")
-Group.create(name: "Bedroom")
-Group.create(name: "Office")
-Group.create(name: "Electronics")
-Group.create(name: "Media")
-Group.create(name: "Appliances")
+# Group.create(name: "Kitchen")
+# Group.create(name: "Living Room")
+# Group.create(name: "Bedroom")
+# Group.create(name: "Office")
+# Group.create(name: "Electronics")
+# Group.create(name: "Media")
+# Group.create(name: "Appliances")
