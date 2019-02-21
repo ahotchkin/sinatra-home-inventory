b46cdcf99f0662fecd8a7facb7b13eb19b69a081
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-02-21 03:23:57 +0000

    Update Slugify module to account for items of the same name that belong to different users

--- app/models/concerns/slugify.rb
+++ app/models/concerns/slugify.rb
@@ -2,13 +2,13 @@ module Slugify
 
   module ClassMethods
     def find_by_slug(slug)
-      self.find { |name| name.slug == slug }
+      self.find { |name| name.slug + name.id.to_s == slug + name.id.to_s }
     end
   end
 
   module InstanceMethods
     def slug
-      self.name.gsub(/\s/, "-").gsub(/[^0-9A-Za-z\-]/, '').downcase
+      self.name.gsub(/\s/, "-").gsub(/[^0-9A-Za-z\-]/, '').downcase + self.id.to_s
     end
   end
 
