d8aabb74924439b6871b6c985912de8341aeab8b
ahotchkin <ahotchk1@yahoo.com>
2019-08-11 22:28:46 -0400

    Add stylesheet and classes to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :views, "app/views"
+    set :public_dir, "public"
     enable :sessions
     set :session_secret, "password_security"
     use Rack::Flash
